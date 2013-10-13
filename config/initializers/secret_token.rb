1655887636453f9b55a7399c663049b0da153eff
Chris Martin <chris@localhost>
2013-10-13 17:26:13 +0100

    Initial commit: only Gemfile and .gitignore have changed from the stock set of files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '3f80a3b540a562654e7f84ad396926499ce762ec2d989cf81fc085301d9f6c9ed6de6529dfb9c2938d4b54f84b886ae334bdceedb4ddc888d9ab0f0f0736de72'
